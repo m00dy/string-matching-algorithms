d4e570715f76811d7c34f4cd5359849f0090a939
Eren Yagdiran <erenyagdiran@gmail.com>
2015-03-11 17:17:55 +0000

    BruteForce Fixed

--- brute_force.py
+++ brute_force.py
@@ -11,21 +11,25 @@ def brute_force(pattern,subject):
   m = len(subject)
   n = len(pattern)
   j = 0
-  for x in subject:
-    if x in ['\n','\r'] or ord(x) == 10:
+  for ch in subject:
+    if ch == '\n':
       pass
-    if x in ['a','t','g','c','A','T','G','C']:
-      if x == pattern[j]:
+
+    if ch in ['C','G','T','A']:
+      if ch == pattern[j]:
         j = j + 1
       else:
         j = 0
-    if j == n-1:
+    if j == n:
       return 1
-  return -1     
+
+  return -1
+
 
 file = open('data/genome.dat','rb')
 subject = file.read()
-pattern = 'CAAGCAATCCTCCTGCCTCAGCCTCACAAG'
+
+pattern = 'CACGTTGCAGTGCACACCTGTAGTCCGAGCTACTGGGGAGGCTAAGCCTGGAGGATCACTTGAGTCTGT'
 
 print brute_force(pattern,subject)
 
