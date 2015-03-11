e1d385a8c5cb874c8c15bc4f9cc65594b54ed29b
Eren Yagdiran <erenyagdiran@gmail.com>
2015-03-11 15:11:30 +0000

    Brute Force algorithm now fixed

--- brute_force.py
+++ brute_force.py
@@ -7,38 +7,26 @@ import timeit
 Instead of mmap, I spared additional swap space to fill 2gb bulk data.
 """
 
-def wrapper(func, *args, **kwargs):
-  def wrapped():
-    return func(*args, **kwargs)
-  return wrapped
-
-
 def brute_force(pattern,subject):
+  m = len(subject)
+  n = len(pattern)
   j = 0
   for x in subject:
-    if x in ['a','c','t','g','A','C','T','G']:
-       if x == pattern[j]:
-         j=j+1
-       else:
-         j=0
-    if j == len(pattern)-1:
-      print "Found"
+    if x in ['\n','\r'] or ord(x) == 10:
+      pass
+    if x in ['a','t','g','c','A','T','G','C']:
+      if x == pattern[j]:
+        j = j + 1
+      else:
+        j = 0
+    if j == n-1:
       return 1
-  print "Not Found"
-  return -1
+  return -1     
 
-if len(sys.argv) < 2:
-   print "Not enough arguments"
-else:
-   pattern = sys.argv[1]
-   print "looking for " + pattern
-   print "starting to read file"
-   file = open('data/genome.dat','rb')
-   subject = file.read()
-   print "File read ends"
-   print "Size of pattern " + str(len(pattern))
-   print "Size of subject " + str(len(subject))
-   print brute_force(pattern,subject)
-   #wrapped = wrapper(brute_force, pattern,subject)
-   #print timeit.timeit(wrapped,number=1)
+file = open('data/genome.dat','rb')
+subject = file.read()
+pattern = 'CAAGCAATCCTCCTGCCTCAGCCTCACAAG'
+
+print brute_force(pattern,subject)
+
 
